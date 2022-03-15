7104f3f3b1cc2e1b6161b6d77c9cb8bc8a57ad3a
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-03-15 18:59:43 +0300

    Tags And ID Selector

---
+++ 04-WhatIsTheDom/04-EtiketAndID.js
@@ -0,0 +1,17 @@
+// Dom  = etiket ve id ile öğe seçimi
+
+// let title = document.getElementById('title');
+// Değişen Bilgi
+// title.innerHTML = "Değişen Bilgi";
+// console.log(title.innerHTML);
+
+// Yukarıdaki yöntem çok daha eski ve keyifsiz.
+
+// querySelector gerekli olan yapıyı bize çok rahat sağlıyor Resmen CSS yazar gibi
+let link = document.querySelector("#kodluyoruzLink");
+console.log(link.innerHTML);
+link.innerHTML +=" degisti";
+link.style.color = "red";
+link.clas
+
+
