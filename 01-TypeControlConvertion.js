a6d1472f4798da90c9cb1121758760cbb590d15d
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-03-15 15:43:03 +0300

    first commit

---
+++ 01-TypeControlConvertion.js
@@ -0,0 +1,16 @@
+let price = 111;
+let stringPrice = "111";
+let hasPassword = true;
+
+console.log(
+    "price :", typeof(price)
+);
+
+console.log(
+    "String Price :",typeof(stringPrice)
+);
+
+console.log(
+    "has password :",typeof(hasPassword)
+);
+
