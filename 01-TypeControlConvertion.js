f08a6a8a4be7cad8b6cf7e195ee4439fae134270
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-03-15 15:55:24 +0300

    Type Control and Convertion

--- 01-TypeControlConvertion.js
+++ 01-TypeControlConvertion.js
@@ -3,14 +3,38 @@ let stringPrice = "111";
 let hasPassword = true;
 
 console.log(
-    "price :", typeof(price)
+    "price :", typeof (price)
 );
 
 console.log(
-    "String Price :",typeof(stringPrice)
+    "String Price :", typeof (stringPrice)
 );
 
 console.log(
-    "has password :",typeof(hasPassword)
+    "has password :", typeof (hasPassword)
 );
 
+// string bilgileri int ve float'a dönüştürmek
+// bunun için 2 yol var = parseInt ve Number
+// parseInt içinde harf olsada çevirir.
+// number ise sadece rakam varsa çevirir.
+let number1 = "11";
+console.log("number1:", typeof (parseInt(number1)));
+
+let number2 = "11px";
+number2 = parseInt(number2); // ilginç bir şekilde bunu number olarak tekrar düzenledi (px nereye gitti :D)
+console.log("number2 :", number2, typeof (number2));
+
+let number3 = "11"
+number3 = Number(number3);
+console.log("number3 :", number3, typeof (number3));
+
+let number4 = "11.4px";
+number4 = Number(number4);
+console.log("number4 :", number4, typeof (number4));
+
+
+// Number veri tipini String'e dönüştürmek.
+let number5 = 55;
+number5 = number5.toString();
+console.log(number5, typeof (number5));
