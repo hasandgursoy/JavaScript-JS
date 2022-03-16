71a209f571ac6b2bf6e857131761285a4806ddaf
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-03-16 15:18:26 +0300

    Ternary

---
+++ 05-ComparisonAndCondition/05-Ternary.js
@@ -0,0 +1,5 @@
+let userName = prompt("Kullanci Bilginiz Yazınız");
+console.log(userName);
+let info = document.querySelector("#info");
+
+userName === "hasan" ? info.innerHTML = "Kodluyoruz 101 e hoşgeldin": info.innerHTML="Defol git karşim";
